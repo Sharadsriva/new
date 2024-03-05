70ba868995ac9a98831f4ea21f847054203275b5
sharadsriva <srivastavasharad208@gmail.com>
2024-03-05 14:48:28 +0530

    deleted unwanted file

--- sharad.java
+++ sharad.java
@@ -1,12 +1,6 @@
 class demo {
-    public void Sum {
-        int a,b,c;
-        Scanner obj= Scanner (System.in);
-        System.out.println ("entwer the number");
-        a=obj.nextInt;
-        b=obj.nextInt;
-        c=a+b;
-        System.out.println("add"+c);
+    public static void main (String[] args){
+        System.out.pritln("hellow");
 
 
     }
