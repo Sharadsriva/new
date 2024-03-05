783cd67fc3c7ea7ba3ae20b5d1a147e1b2a66180
sharadsriva <srivastavasharad208@gmail.com>
2024-03-05 14:42:27 +0530

    deleted unwanted files

---
+++ sharad.java
@@ -0,0 +1,13 @@
+class demo {
+    public void Sum {
+        int a,b,c;
+        Scanner obj= Scanner (System.in);
+        System.out.println ("entwer the number");
+        a=obj.nextInt;
+        b=obj.nextInt;
+        c=a+b;
+        System.out.println("add"+c);
+
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args)
-    {    int res;
+    public static void main(String[] args) {
+        int res;
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter the number whose multiplication is to be find");
         int vari = scanner.nextInt();
         for (int i = 1; i <= 10; i++) {
-            res =vari*i;
-            System.out.println(res );
+            res = vari * i;
+            System.out.println(res);
         }
     }
+        public void Sum{
+        int a,b,c;
+        System.out.println ("entwer the number");
+        a=scanner.nextInt();
+        b=scanner.nextInt();
+        c=a+b;
+        System.out.println("add"+c);
+    }
+
 }
